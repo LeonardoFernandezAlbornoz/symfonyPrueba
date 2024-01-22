bbf67048eab081a19bb88a4f359c1fc31007cbf6
leonardofernandezalbornoz <a22leonardofa@iessanclemente.net>
2024-01-22 13:30:53 +0100

    fifth

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController{
   
-    #[Route("/")]
-    function homepage(){
+    #[Route("/",name:"app_homepage")]
+    function homepage(Environment $twig){
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
             ['song' => 'Waterfalls', 'artist' => 'TLC'],
@@ -20,11 +22,11 @@ class VinylController extends AbstractController{
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
         dump($tracks);
-
-        return $this->render("vinyl/homepage.html.twig",["genre"=>"PB & Jams","tracks"=>$tracks]);
+        $http=$twig->render("vinyl/homepage.html.twig",["title"=>"PB & Jams","tracks"=>$tracks]);
+        return new Response($http);
     }
 
-    #[Route("/browse/{slug}")]
+    #[Route("/browse/{slug}",name:"app_browse")]
     function browse(string $slug =null): Response
     {
        if($slug) {
